dd0abe0d613a2aca96efc68d5a1f801c6bb9f30c
vHrqO <120627599+vHrqO@users.noreply.github.com>
2022-12-28 15:46:57 +0800

    hotfix

---
+++ js/utility.js
@@ -0,0 +1,32 @@
+﻿
+// scrolls the page to the top
+function js_scrollToTop() {
+    
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+}
+
+
+// scrolls the page to the id
+function js_scrollToId(id) {
+
+    const element = document.getElementById(id);
+
+    if (element instanceof HTMLElement) {
+
+        element.scrollIntoView
+            (
+                {
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest"
+                }
+
+        );
+        
+        window.location.hash = id;
+
+    }
+
+}
+
